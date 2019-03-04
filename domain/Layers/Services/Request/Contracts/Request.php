4c873fc5958fb01cfc0951b60f31103107cec950
TheP6 <andriy.pentsak@gmail.com>
2019-03-04 08:10:13 +0200

    Fixes to RequestInterface

--- domain/Layers/Services/Request/Contracts/Request.php
+++ domain/Layers/Services/Request/Contracts/Request.php
@@ -5,9 +5,11 @@ namespace Domain\Layers\Services\Request\Contracts;
 //isolation from third-party libraries
 interface Request
 {
-    public function replace(array $values);
+    public function replace(array $input);
+
+    public function all();
 
     public function set(string $key, $value);
 
-    public function get(string $key = null);
+    public function get(string $key = null, $default = null);
 }
