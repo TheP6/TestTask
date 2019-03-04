3f3ff973b05cfbf75549ac4464f6e4f6b19a5cc6
TheP6 <andriy.pentsak@gmail.com>
2019-03-04 10:31:05 +0200

    Setted up AppSeviceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,46 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+//interfaces
+use Domain\Layers\Data\Contracts\Repositories\BookRepository as BookRepositoryContract;
+use Domain\Layers\Data\Contracts\Repositories\PublisherRepository as PublisherRepositoryContract;
+use Domain\Layers\Data\Contracts\Repositories\GenreRepository as GenreRepositoryContract;
+use Domain\Layers\Data\Contracts\Repositories\AuthorRepository as AuthorRepositoryContract;
+
+use Domain\Layers\Services\Business\Contracts\BookService as BookServiceContract;
+
+use Domain\Layers\Services\Validation\Contracts\Validator as ValidatorContract;
+
+//classes
+use Domain\Frameworks\Laravel\Data\Repositories\BookRepository;
+use Domain\Frameworks\Laravel\Data\Repositories\PublisherRepository as PublisherRepository;
+use Domain\Frameworks\Laravel\Data\Repositories\GenreRepository as GenreRepository;
+use Domain\Frameworks\Laravel\Data\Repositories\AuthorRepository as AuthorRepository;
+
+use Domain\Layers\Services\Business\Contracts\BookService as BookService;
+
+use Domain\Frameworks\Laravel\Services\Validation\ValidationAdapter as Validator;
+
+
 class AppServiceProvider extends ServiceProvider
 {
+
+    public $singletons = [
+
+        //repositories
+        BookRepositoryContract::class => BookRepository::class,
+        PublisherRepositoryContract::class => PublisherRepository::class,
+        GenreRepositoryContract::class => GenreRepository::class,
+        AuthorRepositoryContract::class => AuthorRepository::class,
+
+        //services
+        BookServiceContract::class => BookService::class,
+    ];
+
+    public $bindings = [
+        ValidatorContract::class => Validator::class,
+    ];
+
     /**
      * Register any application services.
      *
